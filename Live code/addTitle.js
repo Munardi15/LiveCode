80784b60261d0e8422a64913a73fe58e3ba9c2d6
Munardi15 <54164085+Munardi15@users.noreply.github.com>
2019-08-26 15:30:44 +0700

    Add files via upload

---
+++ Live code/addTitle.js
@@ -0,0 +1,5 @@
+function addTittle (person1, person2, person3, person4){
+  console.log('Mr.', person1,  ', ', 'Mr.', person2, ', ', 'Mr.', person3, ', ', 'Mr.',  person4, '.');
+}
+
+addTittle('ardi', 'ardu', 'arde', 'ardo');
